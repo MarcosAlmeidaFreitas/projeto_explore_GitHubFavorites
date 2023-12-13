fef5a5026677077979b4a9b7465d25b0759087ba
Marcos Antonio Almeida Freitas <67704150+MarcosAlmeidaFreitas@users.noreply.github.com>
2023-12-13 19:33:32 -0300

    conculsão do trabalho

--- src/Favorites.js
+++ src/Favorites.js
@@ -1,26 +1,50 @@
-export class GitHubUser{
-  static search(username){
-    const endpoint = `https://api.github.com/users/${username}`
+import { GitHubUser } from "./GitHubUser.js";
 
-    return fetch(endpoint)
-    .then(data => data.json())
-    .then(({login, name, public_repos, followers}) => {})
-  }
-}
 export class Favorites {
   constructor(root){
     this.root = document.querySelector(root);
     this.load();
+
+    this.onadd();
+    //GitHubUser.search('MarcosAntonioAlmeida').then(user =>console.log(user));
   }
 
   load(){
     this.entries = JSON.parse(localStorage.getItem('@github-favorites:')) || [];
   }
 
+  save(){
+    //Depois que ele conveter o array em uma string em formato json ele vai salvar no localStore
+    localStorage.setItem('@github-favorites:', JSON.stringify(this.entries))
+  }
+
+  async add(username){
+    try{
+      const userExists = this.entries.find(entry => entry.login === username);
+      console.log(userExists);
+
+      if(userExists){
+        throw new Error('Usuário já cadastrado!');
+      }else{
+        const user = await GitHubUser.search(username);
+        if(user.login === undefined){
+         throw new Error('Usuário não encontrado!');
+        }
+        this.entries = [user, ...this.entries];
+        this.update();
+        this.save();
+      }
+    }catch(error){
+     alert(error.message);
+    }
+  }
+
+
   delete(user){
     const filteredEntries = this.entries.filter(entry => entry.login !== user.login);
     this.entries = filteredEntries;
     this.update();
+    this.save();
   }
 }
 
@@ -31,6 +55,14 @@ export class FavoritesView extends Favorites{
     this.tbody = this.root.querySelector('table tbody');
 
     this.update();
+  }
+
+  onadd(){
+    const addButton = this.root.querySelector('.search button');
+    addButton.onclick = () => {
+      const { value } = this.root.querySelector('.search input');
+      this.add(value);
+    }
   }
 
   update(){
@@ -44,6 +76,7 @@ export class FavoritesView extends Favorites{
       
       row.querySelector('.user img').src = `https://github.com/${user.login}.png`;
       row.querySelector('.user img').alt = `Imagem do usuário ${user.name}`;
+      row.querySelector('.user a').href = `https://github.com/${user.login}`;
       row.querySelector('.user p').textContent = user.name;
       row.querySelector('.user span').textContent = user.login;
       row.querySelector('.repositories').textContent = user.public_repos;
